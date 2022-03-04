8d5d16e9eecdc9ba8ea885a1491d5eab10895d30
Ranjeet Singh <100189054+ranjeet701728@users.noreply.github.com>
2022-03-04 17:35:29 +0530

    Add files via upload

---
+++ find greater Number in  3.py.c
@@ -0,0 +1,9 @@
+a=input ("enter the number a ")
+b=input ("enter the number b ")
+c=input ("enter the number c ")
+if(a>b):
+    print("a is gretar")
+elif(b>c):
+    print("b is gretar")
+else:
+    print("c is gretar")
